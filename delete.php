dbc872a271b7c2e85d7b19dc418b3df4652c917b
ErenJr <ifantahir@gmail.com>
2022-07-08 20:44:41 +0700

    first commit

---
+++ delete.php
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+<?php
+include("koneksi.php");
+    $id = $_GET['nik'];
+    $sql = "DELETE FROM data_pasien WHERE nik='$id'";
+    $query = mysqli_query($koneksi, $sql);
+    if( $query ){
+        header('Location: index.php');
+    } else {
+        die("gagal menghapus");
+    }
+=======
+<?php
+include("koneksi.php");
+    $id = $_GET['nik'];
+    $sql = "DELETE FROM data_pasien WHERE nik='$id'";
+    $query = mysqli_query($koneksi, $sql);
+    if( $query ){
+        header('Location: index.php');
+    } else {
+        die("gagal menghapus");
+    }
+>>>>>>> f655ee8 (first commit)
+?>
